3d91234957b5eaf83bbc4dc1908abaa5e9bd607b
benjamin.ferrari <benjamin.ferrari@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-22 19:21:28 +0000

    started with an naive bayes implementation in erlang

--- trunk/classify/erlang/bayes.erl
+++ trunk/classify/erlang/bayes.erl
@@ -30,12 +30,3 @@ word_is_hit(Word) ->
 	Hit = count(HitCounts,W) * 2,
 	Miss = count(MissCounts,W),
 	(Hit/length(HitCorpus))/((Hit/length(HitCorpus)) + (Miss/length(MissCorpus)) + 0.001 ).
-
-%% Grahams Plan for Spam:
-%% (let ((g (* 2 (or (gethash word good) 0))) 
-%% 	  (b (or (gethash word bad) 0))) 
-%%  (unless (< (+ g b) 5) 
-%%   (max .01 
-%%    (min .99 (float (/ (min 1 (/ b nbad)) 
-%% 					(+ (min 1 (/ g ngood)) 
-%% 					 (min 1 (/ b nbad)))))))))
