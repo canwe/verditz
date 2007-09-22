680b4ebf4934b346bcf358b1e4237926048d7077
benjamin.ferrari <benjamin.ferrari@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-22 19:21:16 +0000

    started with an naive bayes implementation in erlang

---
+++ trunk/classify/erlang/bayes.erl
@@ -0,0 +1,41 @@
+-module(bayes).
+-compile(export_all).
+
+words(Bin) ->
+    {ok, Words} = regexp:split(binary_to_list(Bin), "[^a-zA-Z]"),
+    lists:map(fun(X) -> X end, lists:filter(fun(X) -> length(X) > 0 end, Words)).
+
+
+wordcounts(Class, Words) ->
+    Dict = ets:new(Class, [set, private]),
+    lists:foreach(fun(X) ->
+        case ets:insert_new(Dict, {list_to_binary(X), 1}) of
+            false -> ets:update_counter(Dict, list_to_binary(X), 1);
+            true -> true
+        end
+    end, Words),
+    Dict.
+
+count(Dict, W) ->
+	case ets:lookup(Dict,W) of 
+		[{_,X}] -> X;
+	    [] -> 0 end.
+
+word_is_hit(Word) ->  
+	W = list_to_binary(Word),
+	HitCorpus = words(list_to_binary("Hallo Welt. Hallo Hallo Hallo Free Software!")),
+	MissCorpus = words(list_to_binary("Penis Enlargement. Free Girls! Free Gold, Free Nazi-Underware Free Free Free Free Free Fish")),
+	HitCounts = wordcounts(good,HitCorpus),
+	MissCounts  = wordcounts(bad,MissCorpus),
+	Hit = count(HitCounts,W) * 2,
+	Miss = count(MissCounts,W),
+	(Hit/length(HitCorpus))/((Hit/length(HitCorpus)) + (Miss/length(MissCorpus)) + 0.001 ).
+
+%% Grahams Plan for Spam:
+%% (let ((g (* 2 (or (gethash word good) 0))) 
+%% 	  (b (or (gethash word bad) 0))) 
+%%  (unless (< (+ g b) 5) 
+%%   (max .01 
+%%    (min .99 (float (/ (min 1 (/ b nbad)) 
+%% 					(+ (min 1 (/ g ngood)) 
+%% 					 (min 1 (/ b nbad)))))))))
