612c894d68ed4b957d6455359e4962219fb58aa6
benjamin.ferrari <benjamin.ferrari@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-22 19:21:16 +0000

    started with an naive bayes implementation in erlang

--- classify/src/at/ac/tuwien/dbai/verditz/classify/ClassifierState.java
+++ classify/src/at/ac/tuwien/dbai/verditz/classify/ClassifierState.java
@@ -3,7 +3,6 @@ package at.ac.tuwien.dbai.verditz.classify;
 import java.util.Set;
 
 import weka.classifiers.Classifier;
-import weka.core.Attribute;
 import weka.core.Instances;
 import weka.filters.Filter;
 
