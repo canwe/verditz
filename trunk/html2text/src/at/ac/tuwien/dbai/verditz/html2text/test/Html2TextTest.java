3f4481dbd79f11cc4acf762e9b48087bcb2bda10
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-28 16:56:06 +0000

    implemented html2text. Html2Text.getText(String url) returns the plain text of a html page behind a URL. it's currently not possible to pass HTML as a String to getText because the htmlparsers' (see htmlparser.sourceforge.net) StringBean does not support this.

---
+++ trunk/html2text/src/at/ac/tuwien/dbai/verditz/html2text/test/Html2TextTest.java
@@ -0,0 +1,36 @@
+package at.ac.tuwien.dbai.verditz.html2text.test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import junit.framework.TestCase;
+import at.ac.tuwien.dbai.verditz.html2text.Html2Text;
+
+public class Html2TextTest extends TestCase {
+	public void testHtml2Text() throws MalformedURLException {
+		String url = new File("data/blog_bookworm_at.html").toURI().toURL()
+				.toString();
+		String text = Html2Text.getText(url);
+		assertTrue(text.contains("blog.bookworm.at"));
+		assertTrue(text.contains("Benjamin Ferrari's Weblog. This blog is still beta."));
+		assertFalse(text.contains("<script type='text/javascript'>"));
+		assertFalse(text.contains("<div class='post'>"));
+		assertFalse(text.contains("<ul>"));
+	}
+
+	public void testMalformedUrl() {
+		try {
+			Html2Text.getText("thisisnourl");
+			fail("Html2Text.getText(\"thisisnourl\") should throw an exception.");
+		} catch (RuntimeException e) {
+		}
+	}
+
+	public void testUnknownHost() {
+		try {
+			Html2Text.getText("http://nonexisting49375.tz");
+			fail("Html2Text.getText(\"http://nonexisting49375.tz\") should throw an exception.");
+		} catch (RuntimeException e) {
+		}
+	}
+}
