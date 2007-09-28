377e1cfe3a4e2b61a386e2dabf9ba189b8bf2881
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-28 21:16:02 +0000

    added project infrastructure for the following projects:
    indexer
    link2contentfeed
    webapp

---
+++ trunk/indexer/src/at/ac/tuwien/dbai/verditz/indexer/Indexer.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.dbai.verditz.indexer;
+
+public class Indexer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
