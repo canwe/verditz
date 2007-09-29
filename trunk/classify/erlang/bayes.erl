8a6cf5ea61b631282d687c2c3a7bef667f9437b0
benjamin.ferrari <benjamin.ferrari@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-29 21:44:52 +0000

    new, concurrent bayes. uses mapreduce for both training files and calculating probabilities

--- trunk/classify/erlang/bayes.erl
+++ trunk/classify/erlang/bayes.erl
@@ -1,129 +1,143 @@
 -module(bayes).
--export([
-		 train/0,
-		 train_hit/1,
-		 train_miss/1, 
-		 train_hitsfile/0,
-		 train_missesfile/0,
-		 init/0,
-		 classify/1,
-		 classify_file/1,
-		 test/0,
-		 word_is_hit/1]).
+-export([train/0, classify/1, test/0, wordcounts/1,testP/1]).
 -define(bias,2).
 -define(min_word_length,3).
 
+-define(hitsDir,"hits").
+-define(missesDir,"misses").
+%% -define(hitsDir,"hits_small").
+%% -define(missesDir,"misses_small").
 
-%-include_lib("xmerl/include/xmerl.hrl").
-%-export([main/1]).
+test() ->
+    lists:foreach(
+	  fun(_N) -> 
+			  L = lists:map(fun(X) -> 
+								{T,_} = timer:tc(bayes,testP,[X]), T end, 
+						[1000, 2000, 5000, 10000, 100000, 1000000]),
+			  io:format("~p~n",[L]) end,
+	  lists:seq(1,100)).
 
-% train_hit:
-% Input: String 
-% Output: void
-% Process:
-% - load ets table from file (if exists)
-% - split string into word => count map
-% - merge word - count map into ets table
-% - save ets table to file
+testP(N)->
+	Hits = wordcounts(?hitsDir),
+	Misses = wordcounts(?missesDir),
+	Misses.
+%    Probabilities = create_probabilities(filter(Hits), filter(Misses), N),
+%	dict:to_list(Probabilities).
 
-% train_miss: like hit ...
-%
-
-% classify_many 
-% Input: Array of Strings (Texte, jeweils erste zeile ist titel/id)
-% Output: map: Id => Probability
-% Process:
-% - foreach Text:
-%     Id = first line
-%     spawn new process
-%     Map = fun({Id,Text}) -> ... {ok,Id,P} end  
-%     Reduce  = fun({Id,Text}) -> ... {ok,[{Id,P}]} end  
-%     
-
-
-train_hit(Text) ->
-	train(hits, Text).
-
-train_miss(Text) ->
-	train(misses, Text).
+filter(WordCounts) -> 
+	dict:filter(fun({_,Count}) -> Count > 5 end, WordCounts).
 
 train() ->
-	train_hitsfile(),
-	train_missesfile(),
+	Hits = wordcounts(?hitsDir),
+	Misses = wordcounts(?missesDir),
+    Probabilities = create_probabilities(Hits, Misses, 10),
 	{bayes,ok}.
+	
 
-train_hitsfile() ->
-	train_hit(binary_to_list(read_file("raw_hits"))).
+get_item(Key, Dict) ->
+	case dict:find(Key,Dict) of
+		error -> 0;
+		{ok, Value} -> Value end.
+					 
+									 
 
-train_missesfile() ->
-	train_miss(binary_to_list(read_file("raw_misses"))).
+create_probabilities(Hits, Misses, numbags) ->
+	KeysHits = lists:sort(dict:fetch_keys(Hits)),
+	KeysMisses = lists:sort(dict:fetch_keys(Misses)),
+	Nhits = length(KeysHits),
+	Nmisses = length(KeysMisses),
+	Words = lists:umerge(KeysHits, KeysMisses),	
+	Bags = split_list_into_bags(Words, numbags),
+	WordDict = ets:new(words,[set,public,named]),
+	phofs:mapreduce(
+	  fun(Pid, Bag) ->
+			  lists:map(
+				fun(Word) -> 
+						Pid ! 
+							{Word,
+							 probability(
+							   get_item(Word, Hits), 
+							   get_item(Word, Misses), 
+							   Nhits,
+							   Nmisses,
+							   Word)} end, 
+				Bag) end,
+	  fun(Key,Values,_A) -> 
+			  [Value | _] = Values,
+			  ets:insert(WordDict,{Key, Value}) end,
+	  nothing, Bags),
+	{bayes, words}.
 
-train(Class, Text) ->
-	Table = create_wordcounts_table(Class,Text),
-	ets:tab2file(Table, atom_to_list(Class)).
+probability(Hit, Miss, Nhits, Nmisses, Word) ->
+	P = (Hit/Nhits)/((Hit/Nhits) + (Miss/Nmisses)),
+	lists:max([lists:min([P,0.99]),0.1]). 
 
-init() ->
-	init_tab('hits'),
-	init_tab('misses'),
-	{bayes,init,ok}.
+	
 
-init_tab(Tab) ->
-	case ets:info(Tab) of
-		undefined -> {ok, _} = ets:file2tab(Tab);
-		_ -> ok
-		end.
-test() ->
-	filelib:is_regular("hits").
+split_list_into_bags(L, N) when length(L) < N ->
+	[L];
 
-
-
-classify_file(Filename) ->
-	classify(binary_to_list(read_file(Filename))).
+split_list_into_bags(L,N) ->
+	{L1, L2} = lists:split(N, L),
+	[L1] ++ split_list_into_bags(L2,N).
 
 classify(Text) ->
-	init(),
-	AllProbabilities = lib_misc:pmap(fun(Word) -> word_is_hit(Word) end, words(Text)),
-	Probabilities = lists:filter(fun interesting_word/1, AllProbabilities),
- 	Product = lists:foldl(fun(A,B)->A*B end,1,Probabilities),
-	Score = Product  / (Product + lists:foldl(fun(A,B)->(1-A)*B end,1,Probabilities)),
-	Score.
+	0.
 
-interesting_word(P) ->
-	(abs(P-0.5) > 0.2).
 
-read_file(Filename) -> 
-	{ok, Bin} = file:read_file(Filename),
-    Bin.
+wordcounts(Directory) ->
+	Files = lib_find:files(Directory, "*", false),
+	dict:from_list(phofs:mapreduce(
+	  fun(Pid, File) -> 
+			  SendWord = fun(Word) -> Pid ! {httpd_util:to_lower(Word), 1} end,
+			  lists:map(SendWord, words_in_string(read_file(File))) end,
+	  fun(Key, Values, A) -> 
+			  [{Key, length(Values)} | A]
+	  end, 
+	  [], Files)).
 
-words(Text) ->
-    {ok, Words} = regexp:split(httpd_util:to_lower(Text), "[^a-zA-Z]"),
-    lists:map(fun(X) -> X end, lists:filter(fun(X) -> length(X) > 0 end, Words)).
+read_file(File) ->
+    case file:read_file(File) of
+		{ok, Bin} -> binary_to_list(Bin);
+		_         -> []
+    end.
 
-create_wordcounts_table(Class, Texts) ->
-	Words = words(Texts),
-    Dict = ets:new(Class, [set, public, named_table]),
-    lists:foreach(fun(X) ->
-        case ets:insert_new(Dict, {list_to_binary(X), 1}) of
-            false -> ets:update_counter(Dict, list_to_binary(X), 1);
-            true -> true
-        end
-    end, Words),
-    Dict.
-
-word_is_hit(Word) ->  
-	case wordcounts(Word) of 
-		{{hits, 0}, {misses, 0}} -> 0.4;
-		{{hits, Hit}, {misses, Miss}} -> 
-			Nhits = ets:info(hits,size),
-			Nmiss = ets:info(misses,size),
-			P = (Hit/Nhits)/((Hit/Nhits) + (Miss/Nmiss)),
-			lists:max([lists:min([P,0.99]),0.1]) 
+words_in_string(String) ->
+	case regexp:split(httpd_util:to_lower(String), "[^a-zA-Z]") of
+		{ok,Words} -> lists:filter(fun(X) -> length(X) > 0 end, Words);
+		_ -> []
 	end.
-wordcounts(W) ->
-	Word = list_to_binary(W),
-	{{hits, wordcount(hits,Word)},{misses, wordcount(misses,Word)}}.
+
+%    
+
 	
-wordcount(Dict, W) ->
-	case ets:lookup(Dict,W) of 
-		[{_,X}] -> X;
-	    [] -> 0 end.
+%time with ets: {47 62 56 04,18}
+% time with dict: 48258832,
+% time with dict and 100 cpus: 76511359
+%% word_is_hit(Word) ->  
+%% 	case wordcounts(Word) of 
+%% 		{{hits, 0}, {misses, 0}} -> 0.4;
+%% 		{{hits, Hit}, {misses, Miss}} -> 
+%% 			Nhits = ets:info(hits,size),
+%% 			Nmiss = ets:info(misses,size),
+%% 			P = (Hit/Nhits)/((Hit/Nhits) + (Miss/Nmiss)),
+%% 			lists:max([lists:min([P,0.99]),0.1]) 
+%% 	end.
+%% wordcounts(W) ->
+%% 	Word = list_to_binary(W),
+%% 	{{hits, wordcount(hits,Word)},{misses, wordcount(misses,Word)}}.
+
+
+
+
+
+%% wordcounts_old(Directory) ->
+%% 	Files = lib_find:files(Directory, "*", false),
+%% 	Dict = ets:new(list_to_atom, [set, public]),
+%% 	phofs:mapreduce(
+%% 	  fun(Pid, File) -> 
+%% 			  F = fun(Word) -> Pid ! {httpd_util:to_lower(Word), 1} end,
+%% 			  lib_misc:foreachWordInFile(File, F) end,
+%% 	  fun(Key, Values, _) -> ets:insert(Dict,{Key, length(Values)}) end, 
+%% 	  nothing, Files),
+%% 	Dict.
