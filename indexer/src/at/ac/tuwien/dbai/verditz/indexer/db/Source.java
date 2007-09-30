1b5cecb988561df9f5ea05931f6ca51c8952930a
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-30 18:45:59 +0000

    more work on indexer
    renamed rails app from 'webapp' to 'verditz_on_rails'

--- indexer/src/at/ac/tuwien/dbai/verditz/indexer/db/Source.java
+++ indexer/src/at/ac/tuwien/dbai/verditz/indexer/db/Source.java
@@ -1,5 +1,15 @@
 package at.ac.tuwien.dbai.verditz.indexer.db;
 
 public class Source {
+	private Integer id;
+
 	private String url;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
 }
--- verditz_on_rails/app/controllers/application.rb
+++ verditz_on_rails/app/controllers/application.rb
@@ -3,5 +3,5 @@
 
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_webapp_session_id'
+  session :session_key => '_verditz_on_rails_session_id'
 end
