224bcf15914cdd34d5f4835c5fcaf1a05f749f5d
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-09-30 18:48:21 +0000

    added some libraries and two classes i forgot to add

---
+++ indexer/src/at/ac/tuwien/dbai/verditz/indexer/IndexerException.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.dbai.verditz.indexer;
+
+public class IndexerException extends Exception {
+
+	private static final long serialVersionUID = 8478616003253937723L;
+
+	public IndexerException() {
+		super();
+	}
+	
+	public IndexerException(Throwable throwable) {
+		super(throwable);
+	}
+}
