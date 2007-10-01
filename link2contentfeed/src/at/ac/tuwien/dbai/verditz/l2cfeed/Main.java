4b89d21f0c4476b688be45df880ab5c8a98862a7
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-10-01 13:55:49 +0000

    added jetty configuration file
    jetty starts on port 14821 and delegates all request to L2CHandler which currently just responds with "Hello L2C!"

--- link2contentfeed/src/at/ac/tuwien/dbai/verditz/l2cfeed/Main.java
+++ link2contentfeed/src/at/ac/tuwien/dbai/verditz/l2cfeed/Main.java
@@ -1,7 +1,25 @@
 package at.ac.tuwien.dbai.verditz.l2cfeed;
 
+import java.io.FileInputStream;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.xml.XmlConfiguration;
+
 public class Main {
+
+	public void start() throws Exception {
+		Server server = new Server();
+		XmlConfiguration configuration = new XmlConfiguration(new FileInputStream("config/jetty.xml"));
+		configuration.configure(server);
+		server.start();
+		server.join();
+	}
+
 	public static void main(String[] args) {
-		
+		try {
+			new Main().start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
