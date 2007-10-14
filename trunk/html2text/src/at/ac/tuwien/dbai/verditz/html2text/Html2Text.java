a07ef313725f4df645b6ff581b9cc944de53b1f9
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-10-14 19:30:34 +0000

    fixed bugs in html2text:
      * scripts are stripped now also when they are not embedded in a comment section
      * character encoding bug
      * limiting concurrent threads in L2CFeed because many web servers limit concurrent connections

--- trunk/html2text/src/at/ac/tuwien/dbai/verditz/html2text/Html2Text.java
+++ trunk/html2text/src/at/ac/tuwien/dbai/verditz/html2text/Html2Text.java
@@ -1,31 +1,55 @@
 package at.ac.tuwien.dbai.verditz.html2text;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 
-import org.htmlparser.beans.StringBean;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasParentFilter;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.filters.NotFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.util.NodeList;
 
 public class Html2Text {
 
-	public static String getText(String url) {
-		StringBean sb = new StringBean();
-		sb.setLinks(false);
-		try {
-			URL myUrl = new URL(url);
-			URLConnection connection = myUrl.openConnection();
-			// StringBean swallows all exceptions so we try to get an
-			// inputstream
-			// because this would throw an exception if the host is unknown
-			connection.getInputStream();
-			sb.setConnection(connection);
-			return sb.getStrings();
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+	public static String getText(String url) throws Exception {
+		NodeFilter textNodeFilter = new NodeClassFilter(TextNode.class);
+		NodeFilter scriptParentFilter = new NotFilter(new HasParentFilter(new TagNameFilter("SCRIPT"), true));
+		NodeFilter styleParentFilter = new NotFilter(new HasParentFilter(new TagNameFilter("STYLE"), true));
+		NodeFilter parentFilter = new AndFilter(scriptParentFilter, styleParentFilter);
+		NodeFilter filter = new AndFilter(textNodeFilter, parentFilter);
+		
+		// WORKAROUND: we need to replace all scripts from the page with a regexp
+		//             because htmlparser doesn't filter scripts (although the
+		//             filter is defined above) if the script isn't embedded in
+		//             a html comment section and contains html markup. (i.e.
+		//             document.write("<b>hello world</b>");)
+		URL myUrl = new URL(url);
+		BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+					myUrl.openStream()));
+
+		StringBuilder builder = new StringBuilder();
+		String inputLine;
+		while ((inputLine = in.readLine()) != null) {
+		    builder.append(inputLine);
 		}
+
+		in.close();
+		
+		String content = builder.toString();
+		content = content.replaceAll("<script.*?</script>", "");
+		
+		Parser parser = new Parser();
+		parser.setInputHTML(content);
+		// END WORKAROUND
+		
+		NodeList list = parser.parse(filter);
+		return list.toHtml();
 	}
 
 	public static void main(String[] args) {
@@ -36,7 +60,7 @@ public class Html2Text {
 		try {
 			String text = Html2Text.getText(args[0]);
 			System.out.println(text);
-		} catch (RuntimeException e) {
+		} catch (Exception e) {
 			System.err.println("Either malformed URL or host is unreachable.");
 		}
 	}
--- trunk/html2text/src/at/ac/tuwien/dbai/verditz/html2text/test/Html2TextTest.java
+++ trunk/html2text/src/at/ac/tuwien/dbai/verditz/html2text/test/Html2TextTest.java
@@ -3,26 +3,35 @@ package at.ac.tuwien.dbai.verditz.html2text.test;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import org.htmlparser.util.ParserException;
+
 import junit.framework.TestCase;
 import at.ac.tuwien.dbai.verditz.html2text.Html2Text;
 
 public class Html2TextTest extends TestCase {
-	public void testHtml2Text() throws MalformedURLException {
+	public void testHtml2Text() throws MalformedURLException, Exception {
 		String url = new File("data/blog_bookworm_at.html").toURI().toURL()
 				.toString();
 		String text = Html2Text.getText(url);
 		assertTrue(text.contains("blog.bookworm.at"));
-		assertTrue(text.contains("Benjamin Ferrari's Weblog. This blog is still beta."));
+		assertTrue(text
+				.contains("Benjamin Ferrari's Weblog. This blog is still beta."));
 		assertFalse(text.contains("<script type='text/javascript'>"));
 		assertFalse(text.contains("<div class='post'>"));
 		assertFalse(text.contains("<ul>"));
+
+		url = new File("data/reuters.html").toURI().toURL().toString();
+		text = Html2Text.getText(url);
+		assertNotNull(text);
+		assertFalse(text.contains("document.write"));
+		assertTrue(text.contains("WASHINGTON (Reuters)"));
 	}
 
 	public void testMalformedUrl() {
 		try {
 			Html2Text.getText("thisisnourl");
 			fail("Html2Text.getText(\"thisisnourl\") should throw an exception.");
-		} catch (RuntimeException e) {
+		} catch (Exception e) {
 		}
 	}
 
@@ -30,7 +39,7 @@ public class Html2TextTest extends TestCase {
 		try {
 			Html2Text.getText("http://nonexisting49375.tz");
 			fail("Html2Text.getText(\"http://nonexisting49375.tz\") should throw an exception.");
-		} catch (RuntimeException e) {
+		} catch (Exception e) {
 		}
 	}
 }
