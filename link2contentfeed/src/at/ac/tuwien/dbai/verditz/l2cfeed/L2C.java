1fd2b2e4fb45b8240616898c42c1e98de6d57e62
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-11-20 15:13:48 +0000

    fixed memory leak. feed entry are now fetched single threaded.

--- link2contentfeed/src/at/ac/tuwien/dbai/verditz/l2cfeed/L2C.java
+++ link2contentfeed/src/at/ac/tuwien/dbai/verditz/l2cfeed/L2C.java
@@ -4,17 +4,10 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.concurrent.Callable;
 
 import at.ac.tuwien.dbai.verditz.html2text.Html2Text;
 
-public class L2C implements Callable<String> {
-
-	private URL url;
-
-	public L2C(URL url) {
-		this.url = url;
-	}
+public class L2C {
 
 	private static boolean isSupportedContentType(final String type) {
 		Collection<String> supportedContentTypes = Arrays.asList(new String[] {
@@ -23,16 +16,16 @@ public class L2C implements Callable<String> {
 		return supportedContentTypes.contains(type);
 	}
 
-	public String call() throws Exception {
+	public static String getText(URL url) throws Exception {
 		URLConnection connection = url.openConnection();
 		connection.setConnectTimeout(5000);
 		if (connection.getContentType() == null) {
 			throw new Exception("content type could not be determinded");
 		}
 		String contentType = connection.getContentType().split(";")[0].trim();
-			
+
 		if (!L2C.isSupportedContentType(contentType))
 			throw new Exception("content type not supported: " + contentType);
-		return Html2Text.getText(this.url.toString());
+		return Html2Text.getText(url.toString());
 	}
 }
