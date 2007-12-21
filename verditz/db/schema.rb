8f81b5bdd2c70cd3ef0ee8f620a97ed76721464b
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-12-21 01:21:30 +0000

    fixed migration

--- verditz/db/schema.rb
+++ verditz/db/schema.rb
@@ -2,17 +2,20 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 7) do
 
   create_table "articles", :force => true do |t|
     t.column "title",        :string,   :limit => 1024
-    t.column "text",         :text
+    t.column "text",         :string,   :limit => 8388608
     t.column "publish_time", :datetime
-    t.column "url",          :string,   :limit => 1024, :null => false
-    t.column "source_id",    :integer,                  :null => false
+    t.column "url",          :string,   :limit => 1024,    :null => false
+    t.column "source_id",    :integer,                     :null => false
   end
 
   create_table "recommendations", :force => true do |t|
+    t.column "user_id",    :integer
+    t.column "article_id", :integer
+    t.column "score",      :decimal
   end
 
   create_table "sources", :force => true do |t|
@@ -20,6 +23,9 @@ ActiveRecord::Schema.define(:version => 4) do
   end
 
   create_table "users", :force => true do |t|
+    t.column "name",            :string
+    t.column "hashed_password", :string
+    t.column "salt",            :string
   end
 
 end
