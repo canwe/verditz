653eb420adb702c7aba464d7565f9dafbd0dbca9
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2007-12-31 14:07:57 +0000

    added new crawler

---
+++ crawler/src/at/ac/tuwien/dbai/verditz/crawler/FeedCrawler.java
@@ -0,0 +1,63 @@
+package at.ac.tuwien.dbai.verditz.crawler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.sun.syndication.fetcher.FeedFetcher;
+import com.sun.syndication.fetcher.FetcherException;
+import com.sun.syndication.fetcher.FetcherListener;
+import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
+import com.sun.syndication.io.FeedException;
+
+public class FeedCrawler {
+	private final static Logger log = Logger.getLogger(FeedCrawler.class);
+
+	private Collection<URL> sources = new ArrayList<URL>();
+	private Collection<FetcherListener> listeners = new ArrayList<FetcherListener>();
+
+	public FeedCrawler() {
+	}
+
+	public FeedCrawler(FetcherListener listener) {
+		this.listeners.add(listener);
+	}
+
+	public FeedCrawler(FetcherListener listener, Collection<URL> sources) {
+		this(listener);
+		this.sources = sources;
+	}
+
+	public void addFetcherListener(FetcherListener listener) {
+		this.listeners.add(listener);
+	}
+	
+	public void addSource(URL source) {
+		this.sources.add(source);
+	}
+
+	public void fetch() {
+		FeedFetcher feedFetcher = new HttpURLFeedFetcher();
+
+		for (FetcherListener listener : this.listeners) {
+			feedFetcher.addFetcherEventListener(listener);
+		}
+
+		for (URL source : sources) {
+			try {
+				feedFetcher.retrieveFeed(source);
+			} catch (IllegalArgumentException e) {
+				log.warn(e);
+			} catch (IOException e) {
+				log.warn(e);
+			} catch (FeedException e) {
+				log.warn(e);
+			} catch (FetcherException e) {
+				log.warn(e);
+			}
+		}
+	}
+}
