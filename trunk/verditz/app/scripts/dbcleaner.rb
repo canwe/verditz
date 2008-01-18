af50a01d6beb388c50c44eb1b233ff0528d64b66
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2008-01-18 14:58:46 +0000

    faking User-Agent because arxiv.org otherwise responds with 403

--- trunk/verditz/app/scripts/dbcleaner.rb
+++ trunk/verditz/app/scripts/dbcleaner.rb
@@ -7,7 +7,7 @@ articles.each { |article|
   uri = URI.parse(article.url)
   begin
     Net::HTTP.start(uri.host, uri.port) { |http|
-      response = http.request_head(uri.request_uri)
+      response = http.request_head(uri.request_uri, initheader={'User-Agent','Mozilla/5.0'})
       if response.kind_of? Net::HTTPRedirection
         p "#{article.url} => #{response['location']}"
         article.url = response['location']
