3d1f96f220e46c3a65c5222b542f7067ebc17697
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2008-01-18 14:18:59 +0000

    added dbcleaner script

---
+++ trunk/verditz/app/scripts/dbcleaner.rb
@@ -0,0 +1,24 @@
+require "../../config/environment"
+require "net/http"
+require "uri"
+
+articles = Article.find(:all)
+articles.each { |article|
+  uri = URI.parse(article.url)
+  begin
+    Net::HTTP.start(uri.host, uri.port) { |http|
+      response = http.request_head(uri.request_uri)
+      if response.kind_of? Net::HTTPRedirection
+        p "#{article.url} => #{response['location']}"
+        article.url = response['location']
+        article.save
+      elsif response.kind_of? Net::HTTPClientError
+        p "#{article.url}: response.class. deleting article."
+        article.destroy
+      end
+    }
+  rescue SocketError
+    p "#{article.url}: host not reachable. deleting article."
+    article.destroy
+  end
+}
