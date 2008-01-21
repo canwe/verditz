0b786d5d314e48074a47543c29707616554c8119
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2008-01-21 12:13:19 +0000

    faking User-Agent header when resolving redirects now

--- trunk/indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
+++ trunk/indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
@@ -8,6 +8,7 @@ public class RedirectResolver {
 	public static URL resolve(URL url) throws IOException {
 		HttpURLConnection.setFollowRedirects(false);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
 		String redirect = conn.getHeaderField("Location");
 		if (redirect != null) {
 			return new URL(redirect);
