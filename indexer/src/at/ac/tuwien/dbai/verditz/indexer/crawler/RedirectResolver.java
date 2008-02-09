1e6f7dc6a77373a172a6e9d16e032b587fb68a2a
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2008-02-09 14:00:19 +0000

    set read timeout in RedirectResolver

--- indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
+++ indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
@@ -10,6 +10,7 @@ public class RedirectResolver {
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
 		conn.setConnectTimeout(5000);
+		conn.setReadTimeout(5000);
 		String redirect = conn.getHeaderField("Location");
 		if (redirect != null) {
 			return new URL(redirect);
