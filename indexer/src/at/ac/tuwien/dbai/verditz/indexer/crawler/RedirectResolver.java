0d7e60841150972c6c1c055de24cb2ffc7ca4720
thomas.hammerl <thomas.hammerl@3bd2ebef-9a38-0410-ac46-33526eb0987a>
2008-02-09 13:12:27 +0000

    set connection timeout to five seconds in RedirectResolver

--- indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
+++ indexer/src/at/ac/tuwien/dbai/verditz/indexer/crawler/RedirectResolver.java
@@ -9,6 +9,7 @@ public class RedirectResolver {
 		HttpURLConnection.setFollowRedirects(false);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+		conn.setConnectTimeout(5000);
 		String redirect = conn.getHeaderField("Location");
 		if (redirect != null) {
 			return new URL(redirect);
